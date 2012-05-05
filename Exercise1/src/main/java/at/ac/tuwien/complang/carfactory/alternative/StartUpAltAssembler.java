fe2563aab3314856f6793e7b2a819633d102ba50
Patonai <patonaipeter@gmail.com>
2012-05-05 04:18:19 +0200

    message test

---
+++ Exercise1/src/main/java/at/ac/tuwien/complang/carfactory/alternative/StartUpAltAssembler.java
@@ -0,0 +1,14 @@
+package at.ac.tuwien.complang.carfactory.alternative;
+
+import at.ac.tuwien.complang.carfactory.alternative.businesslogic.AltAssembler;
+
+public class StartUpAltAssembler {
+
+	
+	public static void main(String[] args) {
+		
+		
+		AltAssembler a = new AltAssembler();
+		
+	}
+}
---
+++ Exercise1/src/main/java/at/ac/tuwien/complang/carfactory/alternative/businesslogic/AltAssembler.java
@@ -0,0 +1,46 @@
+package at.ac.tuwien.complang.carfactory.alternative.businesslogic;
+
+import javax.jms.Connection;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Queue;
+import javax.jms.Session;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import at.ac.tuwien.complang.carfactory.alternative.constants.QueueConstants;
+
+public class AltAssembler {
+
+	private Connection connection = null;
+	private Session session;
+	
+	public AltAssembler() {
+	
+		
+		//test get Motor
+		
+		ActiveMQConnectionFactory conFac = new ActiveMQConnectionFactory();
+        try {
+        	
+        	
+		connection = conFac.createConnection();
+		connection.start();
+		session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		Queue q = session.createQueue(QueueConstants.MOTORQUEUE);
+		MessageConsumer myMsgConsumer = session.createConsumer(q);
+		
+		Message msg = myMsgConsumer.receive();
+		
+		System.out.println("Message received "+ msg.toString());
+	
+		connection.close();
+        } catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+
+}
