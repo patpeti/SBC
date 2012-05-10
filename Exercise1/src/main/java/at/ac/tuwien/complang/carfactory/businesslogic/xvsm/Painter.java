e6bbdf27ce6b92ced53b9ff41ae2cb801592c741
Sebastian Geiger <sbastig@gmx.net>
2012-05-10 13:48:03 +0200

    Fix a typo

--- Exercise1/src/main/java/at/ac/tuwien/complang/carfactory/businesslogic/xvsm/Painter.java
+++ Exercise1/src/main/java/at/ac/tuwien/complang/carfactory/businesslogic/xvsm/Painter.java
@@ -22,7 +22,7 @@ public class Painter extends SpaceUtil {
 
 	/**
 	 * TODO:
-	 * 1. Connect to the mozard space
+	 * 1. Connect to the Mozard space
 	 * 2. Load a Body (which is not yet painted) or an
 	 *    assembled car (which is not yet painted)
 	 * 3. Paint the Body or the Body associated with the car object
@@ -72,7 +72,6 @@ public class Painter extends SpaceUtil {
 				writeBodyIntoSpace(b);
 			}
 		}
-		
 	}
 
 	private void writeBodyIntoSpace(Body b) {
