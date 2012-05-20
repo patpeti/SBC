5ae96109568dc06de3289a757f4b241c6d2e939b
Sebastian Geiger <sbastig@gmx.net>
2012-05-20 22:35:56 +0200

    IProducer: Add javadoc

--- Exercise1/src/main/java/at/ac/tuwien/complang/carfactory/application/xvsm/IProducer.java
+++ Exercise1/src/main/java/at/ac/tuwien/complang/carfactory/application/xvsm/IProducer.java
@@ -1,12 +1,10 @@
 package at.ac.tuwien.complang.carfactory.application.xvsm;
 
-import at.ac.tuwien.complang.carfactory.ui.xvsm.ISpaceListener;
-
 public interface IProducer {
 	void produce();
 	/**
 	 * The time to produce in seconds
-	 * @return 
+	 * @return The number of seconds it takes to produce the part 
 	 */
 	int timeInSec();
 }
