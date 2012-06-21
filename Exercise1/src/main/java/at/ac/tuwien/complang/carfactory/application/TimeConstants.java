5a632a635fbcd852227c41ce3137ebf123729674
Sebastian Geiger <sbastig@gmx.net>
2012-06-22 01:25:18 +0200

    TimeConstants: Add painter delay

--- Exercise1/src/main/java/at/ac/tuwien/complang/carfactory/application/TimeConstants.java
+++ Exercise1/src/main/java/at/ac/tuwien/complang/carfactory/application/TimeConstants.java
@@ -7,4 +7,8 @@ public class TimeConstants {
 	public static final double MOTOR_TIME_IN_SEC = 0;
 	public static final double WHEEL_TIME_IN_SEC = 0;
 	public static final double BASE_DELAY = 0;
+	/** Half the time is used to take a part and the other half is used to paint it.
+	 * We use this to relax the update intervals of the UI, so that there is no
+	 * flickering, which happens, when a part is taken and written back immediately. */
+	public static final long TIME_TO_PAINT = 0L; //time in milliseconds
 }
